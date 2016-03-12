2d545414e7469437cc83670ed9d3f8b6933eebc6
Leejay Schmidt <leejayschmidt@uofs-10-227-135-114.usask.ca>
2016-03-12 16:06:21 -0600

    Added NumPath

--- FunJavaPrograms/NumPaths.java
+++ FunJavaPrograms/NumPaths.java
@@ -12,33 +12,34 @@ public class NumPaths {
         this.n = n;
         this.m = m;
         this.obstacles = obstacles;
+        this.total = 0;
         System.out.println(this.findPaths());
     }
     private static boolean canGoDown(int n, int m, int r, int c, boolean[][] obstacles) {
         return ((r+1) < n && c < m && !obstacles[r+1][c]);
     }
     private static boolean canGoRight(int n, int m, int r, int c, boolean[][] obstacles) {
-        return (r < n && (c+1) < m && !obstacles[r+1][c]);
+        return (r < n && (c+1) < m && !obstacles[r][c+1]);
     }
-    private static Set<String, Integer> newIdxSet(int row, int col) {
-        Set<String, Integer> s = new HashSet<String, Integer>();
+    private static Map<String, Integer> newIdxMap(int row, int col) {
+        Map<String, Integer> s = new HashMap<String, Integer>();
         s.put("row", new Integer(row));
         s.put("col", new Integer(col));
         return s;
     }
     public int findPaths() {
-        Stack<Set<String, Integer>> s = new Stack<Set<String, Integer>>();
+        Stack<Map<String, Integer>> s = new Stack<Map<String, Integer>>();
         //set will act as dictionary for index in array
-        Set<String, Integer> firstVal = newIdxSet(0, 0);
+        Map<String, Integer> firstVal = newIdxMap(0, 0);
         s.push(firstVal);
         //use depth first search to get ALL possible paths
         while(!s.empty()) {
-            Set<String, Integer> cur = s.pop();
+            Map<String, Integer> cur = s.pop();
             int r = cur.get("row").intValue(), c = cur.get("col").intValue();
             if(r == (n-1) && c == (m-1)) ++total;
             else {
-                if(canGoDown(n, m, r, c, obstacles)) s.push(newIdxSet(r+1, c));
-                if(canGoRight(n, m, r, c, obstacles)) s.push(newIdxSet(r, c+1));
+                if(canGoDown(n, m, r, c, obstacles)) s.push(newIdxMap(r+1, c));
+                if(canGoRight(n, m, r, c, obstacles)) s.push(newIdxMap(r, c+1));
             }
         }
         return total;
@@ -62,6 +63,10 @@ public class NumPaths {
         n.changeVals(5, 6, boolArray);
         boolArray[4][2] = true;
         boolArray[3][3] = true;
+        n.changeVals(5, 6, boolArray);
+        boolArray[2][2] = true;
+        boolArray[1][2] = true;
+        boolArray[0][2] = true;
         n.changeVals(5, 6, boolArray);
     }
 }
